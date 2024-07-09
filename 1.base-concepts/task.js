d2b77020841663f68529e47aa9cd27a438af1e81
Oksana Danilova <ilimlaw38@gmail.com>
2024-07-09 20:30:11 +0800

    Задание к лекции Основные понятия готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,32 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = b ** 2 - 4 * a * c;
+
+	switch (d) {
+		case d > 0:
+			arr.push((-b + Math.sqrt(d)) / (2 * a));
+			arr.push((-b - Math.sqrt(d)) / (2 * a));
+			break;
+
+		case d === 0:
+			arr.push(-b / (2 * a));
+			break;
+
+		default:
+			arr = [];
+			break;
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let monthlyPercent = percent / 100 / 12;
+	let loanBody = amount - contribution;
+	let monthlyPayment = loanBody * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1)));
+	let totalAmount = monthlyPayment * countMonths;
+
+	return +totalAmount.toFixed(2);
 }

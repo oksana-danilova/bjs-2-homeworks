2f692caa1f7f7941aa46ce30ecd109a8446ea69b
Oksana Danilova <ilimlaw38@gmail.com>
2024-07-11 19:00:26 +0800

    Первое задание готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,19 +4,13 @@ function solveEquation(a, b, c) {
 	let arr = [];
 	let d = b ** 2 - 4 * a * c;
 
-	switch (d) {
-		case d > 0:
-			arr.push((-b + Math.sqrt(d)) / (2 * a));
-			arr.push((-b - Math.sqrt(d)) / (2 * a));
-			break;
-
-		case d === 0:
-			arr.push(-b / (2 * a));
-			break;
-
-		default:
-			arr = [];
-			break;
+	if (d > 0) {
+		arr.push((-b + Math.sqrt(d)) / (2 * a));
+		arr.push((-b - Math.sqrt(d)) / (2 * a));
+	} else if (d === 0) {
+		arr.push(-b / (2 * a));
+	} else {
+		arr = [];
 	}
 
 	return arr;

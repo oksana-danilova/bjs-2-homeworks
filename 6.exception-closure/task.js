1d2b867d2ea6ba3930ff2183328663351b3369bb
Oksana Danilova <ilimlaw38@gmail.com>
2024-07-31 21:51:10 +0800

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,55 @@
-﻿
+﻿//Задача 1. Форматтер чисел
+
+function parseCount(value) {
+	let number = Number.parseFloat(value);
+	if (Number.isNaN(number)) {
+		throw new Error("Невалидное значение");
+	}
+	return number;
+}
+
+function validateCount(value) {
+	try {
+		return parseCount(value);
+	} catch (error) {
+		return error;
+	}
+}
+
+
+//Задача 2. Треугольник
+
+class Triangle {
+	constructor(a, b, c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		if ((this.a + this.b) < this.c || (this.a + this.c) < this.b || (this.b + this.c) < this.a) {
+			throw new Error('Треугольник с такими сторонами не существует');
+		}
+	}
+
+	get perimeter() {
+		return this.a + this.b + this.c;
+	}
+
+	get area() {
+		let p = this.perimeter / 2;
+		return +(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (error) {
+		return {
+			get perimeter() {
+				return 'Ошибка! Треугольник не существует';
+			},
+			get area() {
+				return 'Ошибка! Треугольник не существует';
+			}
+		};
+	}
+}
